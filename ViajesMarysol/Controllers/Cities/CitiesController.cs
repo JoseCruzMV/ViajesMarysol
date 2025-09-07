150b652758d8600cc75a54dd2f43e56b56410aaa
josecruz <josecruzmv@outlook.com>
2025-09-07 13:40:34 -0600

    Creado el controlador para las operaciones CRUD con su Index y agregado a la barra de navegacion

---
+++ ViajesMarysol/Controllers/Cities/CitiesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ViajesMarysol.Data;
+using ViajesMarysol.Mappers.Interfaces;
+using ViajesMarysol.ViewModels;
+
+namespace ViajesMarysol.Controllers.Cities;
+
+[Authorize(Roles = "Admin")]
+public class CitiesController(
+    ViajesMarysolDBContext context,
+    ITourMapper tourMapper
+    ) : Controller
+{
+    private readonly ViajesMarysolDBContext _context = context;
+    private readonly ITourMapper _tourMapper = tourMapper;
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        List<CityViewModel> cities = _context.Cities
+            .Select(c => _tourMapper.CityModelToViewModel(c))
+            .ToList();
+        return View(cities);
+    }
+}
