6e423be4f8b95996fd6fc62bec571d0fedc6383c
josecruz <josecruzmv@outlook.com>
2025-09-08 18:36:23 -0600

    Creado el action method para mostrar Mis Tours a los usuarios registrados

---
+++ ViajesMarysol/Controllers/Dashboard/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViajesMarysol.Data;
+using ViajesMarysol.Models.Users;
+
+namespace ViajesMarysol.Controllers.UserDashboard;
+
+[Authorize]
+public class DashboardController(
+        ViajesMarysolDBContext context,
+        UserManager<ApplicationUser> userManager
+    ) : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly ViajesMarysolDBContext _context = context;
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var userId = _userManager.GetUserId(User);
+
+        var userTours = await _context.UserTours
+            .Where(ut => ut.UserId == userId)
+            .Include(ut => ut.Tour)
+            .ThenInclude(t => t.Cities)
+            .Select(ut => ut.Tour)
+            .ToListAsync();
+
+        return View(userTours);
+    }
+}
